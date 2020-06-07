330f7169fcc4925ec95a31f55e4e7af2bcde53ee
alanpcao@gmail.com <alanpcao@gmail.com>
2020-06-06 18:40:45 -0700

    Initial Commit

---
+++ Flix-CodePath-2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix-CodePath-2020
+//
+//  Created by Alan Cao on 6/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
