2860a1f9ec5430b0e1e8a7313b21a4595e16f327
alanpcao@gmail.com <alanpcao@gmail.com>
2020-06-14 11:20:54 -0700

    Finish movie details controller

--- Flix-CodePath-2020/Controller/MovieDetailsViewController.swift
+++ Flix-CodePath-2020/Controller/MovieDetailsViewController.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
     
+    @IBOutlet var backdropImageView: UIImageView!
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieDateLabel: UILabel!
+    @IBOutlet var movieDescriptionLabel: UILabel!
+    
     var movie: [String: Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let baseURL = "https://image.tmdb.org/t/p/w500"
+        let imageURL = movie["poster_path"] as? String ?? ""
+        let backdropImageURL = movie["backdrop_path"] as? String ?? ""
+        if let backdropImageURL = URL(string: baseURL + backdropImageURL) {
+            backdropImageView.af_setImage(withURL: backdropImageURL)
+        }
+        if let posterImageURL = URL(string: baseURL + imageURL) {
+            posterImageView.af_setImage(withURL: posterImageURL)
+        }
+        
+        movieTitleLabel.text = movie["title"] as? String
+        movieDateLabel.text = movie["release_date"] as? String
+        movieDescriptionLabel.text = movie["overview"] as? String
     }
     
 
